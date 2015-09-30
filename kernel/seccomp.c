20e10cdfcc47a0f36d37a62cf9602ce69e000b21
Kees Cook <keescook@chromium.org>
2015-09-30 21:50:14 +0530

    seccomp: create internal mode-setting function
    In preparation for having other callers of the seccomp mode setting
    logic, split the prctl entry point away from the core logic that performs
    seccomp mode setting.
    
    Change-Id: Ifbbba54931806d62e2d7fa65d0eb303a147ea021
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Andy Lutomirski <luto@amacapital.net>
    Git-commit: 2a30a4386e4a7e1283157c4cf4cfcc0306b22ac8
    Git-repo: https://www.codeaurora.org/cgit/external/gigabyte/kernel/msm
    Signed-off-by: Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -780,13 +780,13 @@ SYSCALL_DEFINE3(seccomp, unsigned int, op, unsigned int, flags,
 }
 
 /**
- * prctl_set_seccomp: configures current->seccomp.mode
+ * seccomp_set_mode: internal function for setting seccomp mode
  * @seccomp_mode: requested mode to use
  * @filter: optional struct sock_fprog for use with SECCOMP_MODE_FILTER
  *
  * Returns 0 on success or -EINVAL on failure.
  */
-long prctl_set_seccomp(unsigned long seccomp_mode, char __user *filter)
+static long seccomp_set_mode(unsigned long seccomp_mode, char __user *filter)
 {
 	unsigned int op;
 	char __user *uargs;
@@ -811,4 +811,16 @@ long prctl_set_seccomp(unsigned long seccomp_mode, char __user *filter)
 
 	/* prctl interface doesn't have flags, so they are always zero. */
 	return do_seccomp(op, 0, uargs);
+}
+
+/**
+ * prctl_set_seccomp: configures current->seccomp.mode
+ * @seccomp_mode: requested mode to use
+ * @filter: optional struct sock_fprog for use with SECCOMP_MODE_FILTER
+ *
+ * Returns 0 on success or -EINVAL on failure.
+ */
+long prctl_set_seccomp(unsigned long seccomp_mode, char __user *filter)
+{
+	return seccomp_set_mode(seccomp_mode, filter);
 }
